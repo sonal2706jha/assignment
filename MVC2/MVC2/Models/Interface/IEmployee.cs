c53895cedad5042b1618bd8e5d86098403848992
sonal2706jha <sonal.jha@tothenew.com>
2020-04-21 04:26:44 +0530

    MVC2

---
+++ MVC2/MVC2/Models/Interface/IEmployee.cs
@@ -0,0 +1,17 @@
+﻿using MVC2.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC2.Models.Interface
+{
+    interface IEmployee
+    {
+       
+            IEnumerable<Employee> GetEmployees();
+            bool InsertEmployee(Employee employee);
+        
+    }
+}
